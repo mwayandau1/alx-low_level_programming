5735716d632ee4ee163181af7eaafa073c84cc28
mwayandau1 <mosesayandau1@gmail.com>
2022-07-04 12:43:00 +0000

    My first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog: New type struct
+ * @name : String name
+ * @age : float age
+ * @owner :String owner
+ *
+ * Return: no return
+ */
+
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+}

bd13275692bcf004f891bf67c4e384152d247dbc
mwayandau1 <mosesayandau1@gmail.com>
2022-07-04 18:13:25 -0700

    Preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,3 +1,7 @@
+#ifndef MACRO_SIZE
+#define MACRO_SIZE
 
 #define SIZE 1024
+
+#endif
 
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - the is where the code is executed
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
